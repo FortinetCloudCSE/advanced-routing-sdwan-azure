4eed79f79b2bbd6159975977e499546dab88a210
srijaallam <srija.allam10@gmail.com>
2023-10-25 21:38:26 -0400

    updates

--- terraform/vnets.tf
+++ terraform/vnets.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "hub1spoke1-vnet" {
   for_each            = data.azurerm_resource_group.resourcegroup
-  name                = "hub1spoke1-vnet"
+  name                = "Spoke1-vHub1_VNET"
   location            = each.value.location
   resource_group_name = each.value.name
   address_space       = ["192.168.1.0/24"]
@@ -8,7 +8,7 @@ resource "azurerm_virtual_network" "hub1spoke1-vnet" {
 
 resource "azurerm_subnet" "hub1spoke1-subnet1" {
   for_each             = data.azurerm_resource_group.resourcegroup
-  name                 = "hub1spoke1-subnet1"
+  name                 = "Subnet1-Spoke1_SUBNET"
   resource_group_name  = each.value.name
   virtual_network_name = azurerm_virtual_network.hub1spoke1-vnet[each.key].name
   address_prefixes     = ["192.168.1.0/24"]
@@ -17,7 +17,7 @@ resource "azurerm_subnet" "hub1spoke1-subnet1" {
 
 resource "azurerm_virtual_network" "hub1spoke2-vnet" {
   for_each            = data.azurerm_resource_group.resourcegroup
-  name                = "hub1spoke2-vnet"
+  name                = "Spoke2-vHub1_VNET"
   location            = each.value.location
   resource_group_name = each.value.name
   address_space       = ["172.16.1.0/24"]
@@ -25,7 +25,7 @@ resource "azurerm_virtual_network" "hub1spoke2-vnet" {
 
 resource "azurerm_subnet" "hub1spoke2-subnet1" {
   for_each             = data.azurerm_resource_group.resourcegroup
-  name                 = "hub1spoke2-subnet1"
+  name                 = "Subnet1-Spoke2_SUBNET"
   resource_group_name  = each.value.name
   virtual_network_name = azurerm_virtual_network.hub1spoke2-vnet[each.key].name
   address_prefixes     = ["172.16.1.0/24"]
